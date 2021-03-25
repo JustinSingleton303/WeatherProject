0afbf41046cca483ef54a9f4b64e6c7796949faf
JustinSingleton <jsingle9@msudenver.edu>
2021-03-25 13:52:52 -0600

    trying to external api

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=denver&units=imperial&appid=579a6c8b76122bf5319c2f49efbb9867";
+  https.get(url, (resp)=>{
+    console.log(resp);
+  });
+
+  res.send("Hello there!");
+});
+
+app.listen(port, ()=>{
+  console.log("Server running on port 3000 press ^C to stop");
+});
