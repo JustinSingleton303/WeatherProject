85d72b0ec2c7fd8610ea61f36cd555731abb19a7
JustinSingleton <jsingle9@msudenver.edu>
2021-03-27 16:05:05 -0600

    simple user interface added

--- app.js
+++ app.js
@@ -2,34 +2,40 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const port = 3000;
+const bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
   //res.send("Hello there!");
 });
 
-/*
-const query = "denver";
-const apiKey = "579a6c8b76122bf5319c2f49efbb9867";
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=" + apiKey;
-https.get(url, (resp)=>{
-  console.log(resp.statusCode);
-  resp.on("data", (data)=>{
-    const weatherData = JSON.parse(data);
-    const localTemp = weatherData.main.temp;
-    const description = weatherData.weather[0].description;
-    const icon = weatherData.weather[0].icon;
-    const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    console.log(localTemp + " " + description);
-    res.write("<p>We are having " + description + " today.</p>");
-    res.write("<h1>The temperature in " + query + " is " + localTemp +
-             " Degrees fahrenheit.</h1>");
-    res.write("<img src='" + imgUrl + "'</>");
-    res.send();
+app.post("/", (req, res)=>{
+  
+  const query = req.body.cityName;
+  const apiKey = "579a6c8b76122bf5319c2f49efbb9867";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=" + apiKey;
+  https.get(url, (resp)=>{
+    console.log(resp.statusCode);
+    resp.on("data", (data)=>{
+      const weatherData = JSON.parse(data);
+      const localTemp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      console.log(localTemp + " " + description);
+      res.write("<p>We are having " + description + " today.</p>");
+      res.write("<h1>The temperature in " + query + " is " + localTemp +
+               " Degrees fahrenheit.</h1>");
+      res.write("<img src='" + imgUrl + "'</>");
+      res.send();
+    });
   });
 });
+
+/*
+
 */
 
 app.listen(port, ()=>{
